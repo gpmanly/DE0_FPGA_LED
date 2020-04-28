cf2b425e662e753c75d4a0a947aca6ffe17ffe58
Gerfel Philip Gonzales <gp.gerfel@gmail.com>
2020-04-28 11:37:29 +0800

    Initial commit

---
+++ simple_counter.v
@@ -0,0 +1,16 @@
+
+module simple_counter(
+								CLOCK_50,
+								counter_out
+							);
+	
+input 	CLOCK_50;
+output 	[31:0]counter_out;
+reg 		[31:0]counter_out;
+
+always@(posedge CLOCK_50)
+	begin
+		counter_out <= #1 counter_out + 1;
+	end
+
+endmodule
